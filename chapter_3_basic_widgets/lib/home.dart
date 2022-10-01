04f0b31145fcccd562d1d423d9a40a8cd01628b1
eoeefosa <eoeefosa@gmail.com>
2022-10-01 10:58:57 +0100

    added bottomnavigationbar functionality

--- chapter_3_basic_widgets/lib/home.dart
+++ chapter_3_basic_widgets/lib/home.dart
@@ -8,14 +8,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int selectedtab = 0;
+  void onTapItem(int index) {
+    setState(() {
+      selectedtab = index;
+    });
+  }
+
+  List Pages = <Widget>[
+    Container(color: Colors.black),
+    Container(color: Colors.red),
+    Container(color: Colors.blue),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card1'),
-        BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card2'),
-        BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card3'),
-      ]),
+      body: Pages[selectedtab],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card1'),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card2'),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Card3'),
+        ],
+        onTap: onTapItem,
+        selectedItemColor: Colors.green,
+        currentIndex: selectedtab,
+      ),
     );
   }
 }
