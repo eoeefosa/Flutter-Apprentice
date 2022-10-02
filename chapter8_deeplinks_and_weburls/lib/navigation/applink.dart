d0134c5a91afb1f9a85bba86dc129ec279071a91
eoeefosa <eoeefosa@gmail.com>
2022-10-02 14:38:04 +0100

    commented on applink

--- chapter8_deeplinks_and_weburls/lib/navigation/applink.dart
+++ chapter8_deeplinks_and_weburls/lib/navigation/applink.dart
@@ -1,31 +1,56 @@
 // this is your navigation state object
 // it is the intermeidiary btw a "URL string" and your "app state"
+/*
+AppLink is an object that helps store the route information. It helps to parse 
+the URL string to a route and vice versa, converting the route information back
+to a URL string. It essentially encapsulates all the logic that transforms a 
+simple string into a state and back 
+*/
 class Applink {
+  // constants for each URL path
   static const String kHomePath = '/home';
   static const String kOnboardingPath = '/Onboarding';
   static const String kLoginPath = '/Login';
   static const String kProfilePath = '/Profile';
   static const String kItemPath = '/Item';
+  // constants for each of the query parameters you’ll support
   static const String kTabParam = 'tab';
   static const String kIdParam = 'id';
 
+// Store the path of the URL using location
   String? location;
+
+  // Use currentTab to store the tab you want to redirect the user to
   int? currentTab;
+
+  // Store the ID of the item you want to view in itemId
   String? itemId;
+
+  // initialize the app link with the location and the two query parameters
   Applink({this.location, this.currentTab, this.itemId});
 
   // from location
   static Applink fromLocation(String? location) {
+    // decode the URL. URLs often include special characters in their  paths, so
+    //you need to percent-encode the URL path. For example, you’d encode
+    //hello!world to hello%21world
     location = Uri.decodeFull(location ?? '');
+    // Parse the URI for query parameter keys and key-value pairs.
     final uri = Uri.parse(location);
     final params = uri.queryParameters;
+    // Extract the currentTab from the URL path if it exists
     final currentTab = int.tryParse(params[Applink.kTabParam] ?? '');
+    // Extract the itemId from the URL path if it exists
     final itemId = params[kIdParam];
+
+    // Create the AppLink by passing in the query parameters you extract from
+    //the URL string.
     final link = Applink(
       currentTab: currentTab,
       itemId: itemId,
       location: uri.path,
     );
+    // Return the instance of AppLink
     return link;
   }
 
