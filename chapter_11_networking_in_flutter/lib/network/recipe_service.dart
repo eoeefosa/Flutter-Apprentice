056d67c94c2d23f21ac77a86123ee0cc0fb7fe12
eoeefosa <eoeefosa@gmail.com>
2022-12-12 14:00:46 +0100

    added recipeService class

--- chapter_11_networking_in_flutter/lib/network/recipe_service.dart
+++ chapter_11_networking_in_flutter/lib/network/recipe_service.dart
@@ -6,3 +6,18 @@ import 'package:http/http.dart';
 const String apiKey = 'b426e5ec';
 const String apiId = "e197f6eb492a00e1e94acbfaaba87e3e";
 const String apiUrl = 'http://api.edamam.com/search';
+
+// class and function to get the data from the API
+class RecipeService {
+  Future getData(String url) async {
+    print('Calling url: $url');
+    final response = await get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      print(response.statusCode);
+    }
+  }
+  // TODO: Add getRecipes
+}
